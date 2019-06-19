0299102713c51ff61cf2be5b45b5fa63afc26c5b
diegaco <diegoacosta211@gmail.com>
2019-06-19 00:26:52 -0300

    add mdx file to create contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Frontend Masters Gatsby Workshop',
     description: 'A site we built together during a full-day workshop',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/Layout.js'),
+        },
+      },
+    },
+  ],
 };
