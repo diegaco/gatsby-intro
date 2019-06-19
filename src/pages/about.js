0004887261e51c448cd737d9590270e407315bef
diegaco <diegoacosta211@gmail.com>
2019-06-18 23:43:48 -0300

    header component & emotion styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/Layout';
 
 export default () => {
   return (
-    <>
+    <Layout>
       <h1>About Page</h1>
       <p>This is my personal website</p>
       <Link to="/">&larr; go to homepage</Link>
-    </>
+    </Layout>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '../components/Layout';
 import { Link } from 'gatsby';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Hello World</p>
     <Link to="/about">Learn about me &rarr;</Link>
-  </>
+  </Layout>
 );
