1f8331b7ada309a81dd90d6870c9c1d1d3e375a7
diegaco <diegoacosta211@gmail.com>
2019-08-03 00:25:57 -0300

    dealing with case sensitive

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import { MDXRenderer } from 'gatsby-mdx';
-import Layout from '../components/Layout';
-import ReadLink from '../components/ReadLink';
+import Layout from '../components/layout';
+import ReadLink from '../components/readLink';
 
 export const query = graphql`
   query($slug: String!) {
